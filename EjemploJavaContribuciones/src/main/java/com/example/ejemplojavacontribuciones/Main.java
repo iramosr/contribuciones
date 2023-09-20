69a0e321572cbfbff552f6a3c7d31dd919f31666
mmane0 <mmanea01@iescastelar.com>
2023-09-20 19:44:25 +0200

    Actualizacion Java

--- EjemploJavaContribuciones/src/main/java/com/example/ejemplojavacontribuciones/Main.java
+++ EjemploJavaContribuciones/src/main/java/com/example/ejemplojavacontribuciones/Main.java
@@ -1,7 +1,10 @@
 package com.example.ejemplojavacontribuciones;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        scaner sc = new Scaner(System.in)
+        int numero = sc.nextInt();
+        System.out.print(numero*2);
+
     }
 }
